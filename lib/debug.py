2310ae359b8e9cb6cc6ea218f35a89d59af6e29e
bryankiriama <bryankiriama@gmail.com>
2025-11-29 09:31:53 +0300

    used an external library to simplify tasks

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from sqlalchemy_sandbox import Student
+from sqlalchemy_sandbox import Base, Student
 
 engine = create_engine('sqlite:///students.db')
 
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
